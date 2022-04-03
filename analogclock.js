cf5ff099dd1cbb48e82a84c3616c6caae14f85fe
KamalxChoudhary <102873337+KamalxChoudhary@users.noreply.github.com>
2022-04-03 18:01:34 +0530

    Update analogclock.js

--- analogclock.js
+++ analogclock.js
@@ -9,7 +9,7 @@ setInterval(
 time = new Date();
 second = time.getSeconds() * 6;
 minute = time.getMinutes() * 6;
-hour = time.getHours() * 30;
+hour = time.getHours() * 30 + Math.round(minute / 12);
 document.getElementById('second-hand').style.transform = "rotate(" + second + "deg)";
 document.getElementById('minute-hand').style.transform = "rotate(" + minute + "deg)";
 document.getElementById('hour-hand').style.transform = "rotate(" + hour + "deg)";
