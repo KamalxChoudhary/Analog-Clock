f03d6a3d22e9c92c16fe2e18c69679621221259e
KamalxChoudhary <102873337+KamalxChoudhary@users.noreply.github.com>
2022-04-03 17:54:58 +0530

    Add files via upload

---
+++ analogclock.js
@@ -0,0 +1,18 @@
+
+let second = 0;
+let minute = 0;
+let hour = 0;
+let time = new Date();
+
+setInterval(
+  function() {
+time = new Date();
+second = time.getSeconds() * 6;
+minute = time.getMinutes() * 6;
+hour = time.getHours() * 30;
+document.getElementById('second-hand').style.transform = "rotate(" + second + "deg)";
+document.getElementById('minute-hand').style.transform = "rotate(" + minute + "deg)";
+document.getElementById('hour-hand').style.transform = "rotate(" + hour + "deg)";
+  }, 1000
+
+);
